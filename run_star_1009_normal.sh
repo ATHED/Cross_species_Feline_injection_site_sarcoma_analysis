698f697039401e739e17e44232bdc5e4ce809f08
Qi WEI <w.qiathed@me.com>
2020-03-11 18:29:01 -0700

    Add files via upload

---
+++ run_star_1009_normal.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+for name in 1009_normal
+do
+   file_prefix=/nfs1/BIOMED/Ramsey_Lab/sramsey/loehr.feline/fastq_trimmed/${name}
+   /local/cluster/STAR/bin/Linux_x86_64/STAR  --genomeDir /nfs1/BIOMED/Ramsey_Lab/weiq/RNAseq/RNAseqdata/reference_completegenome \
+                                              --sjdbGTFfile /nfs1/BIOMED/Ramsey_Lab/weiq/RNAseq/RNAseqdata/reference_completegenome/Felis_catus.Felis_catus_6.2.87.gtf \
+                                              --readFilesIn ${file_prefix}_R1_val_1.fq \
+					                    ${file_prefix}_R2_val_2.fq \
+                                              --outSAMtype BAM Unsorted SortedByCoordinate \
+                                              --outFilterMultimapNmax 1 \
+                                              --outSAMunmapped Within \
+                                              --quantMode TranscriptomeSAM \
+                                              --twopassMode Basic \
+                                              --runThreadN 12 \
+                                              --outFileNamePrefix  ${name}_star
+done
+
